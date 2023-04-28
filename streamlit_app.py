9ecc8effeadb4db098ed12277a369c2ccfa10c3e
crowtechAI <130277199+crowtechAI@users.noreply.github.com>
2023-04-28 16:59:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,67 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Streamline App</title>
+    <style>
+      /* Style the form */
+      form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 50px;
+      }
+      
+      input[type="text"] {
+        padding: 10px;
+        margin-bottom: 20px;
+        width: 300px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+      }
+      
+      input[type="submit"] {
+        background-color: #4CAF50;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+      
+      input[type="submit"]:hover {
+        background-color: #45a049;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Streamline App</h1>
+    <form>
+      <label for="api-key">API Key:</label>
+      <input type="text" id="api-key" name="api-key" placeholder="Enter your API key">
+      <label for="url">URL:</label>
+      <input type="text" id="url" name="url" placeholder="Enter a URL">
+      <input type="submit" value="Submit">
+    </form>
+    
+    <script>
+      // Get the form element and add an event listener for form submission
+      const form = document.querySelector('form');
+      form.addEventListener('submit', handleSubmit);
+      
+      // Define the handleSubmit function
+      function handleSubmit(event) {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+        
+        // Get the values from the API key and URL input fields
+        const apiKey = document.getElementById('api-key').value;
+        const url = document.getElementById('url').value;
+        
+        // Use the values to make an API request or perform some other action
+        console.log(apiKey, url);
+      }
+    </script>
+  </body>
+</html>
